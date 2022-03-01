ecff3a5506a622768a5b1dfc0bf3770badc30a9f
rivi2000@student.miun.se <rivi2000@student.miun.se>
2022-03-01 14:12:48 +0100

    Adding logic

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,16 @@ import React from "react"
 class TodoItem extends React.Component{
     render(){
         return (
-            <li>{this.props.todo.title}</li>
+            <li>
+            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+            Delete
+            </button>
+            <input
+             type="checkbox"
+             checked={this.props.todo.completed}
+             onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+              /> 
+            </li>
         )
     }
 }
